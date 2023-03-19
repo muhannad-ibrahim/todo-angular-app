4747c4fba4856d968168a397f21ad315aeeff7f3
muhannad ibrahim <muhannad.ibrahim2311@gmail.com>
2023-03-19 14:49:28 +0200

    adding array and check with completed property

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { todos } from './todo'
+import { Todo } from './todoInterface'
 
 @Component({
   selector: 'app-todo',
@@ -9,19 +11,28 @@ export class TodoComponent {
   doneChecked:boolean = false;
   indexChecked:number = -1;
   task:string = "";
-  todoArr: string[]= ["Drink Coffee","Going ITI"];
 
-  deleteArr(index:number) : void{
-    this.todoArr.splice(index,1);
+  todoArr: Todo[] = todos;
+
+  deleteArr(id:number) : void{
+    const objWithIdIndex = this.todoArr.findIndex((obj) => obj.id === id);
+
+    if (objWithIdIndex > -1) {
+      this.todoArr.splice(objWithIdIndex,1);
+    }
   }
 
-  add() : void{
-    if(this.task !== "")
-      this.todoArr.push(this.task);
+  add(index: number) : void{
+    if(this.task !== ""){
+      this.todoArr.push({id:index , todo:this.task , completed:false , userId:1});
+      this.task="";
+    }
   }
 
   done(id:number) : void{
-    this.doneChecked = !this.doneChecked;
-    this.indexChecked = id;
+    this.todoArr.find((obj) => {
+      if(obj.id === id)
+        obj.completed = !obj.completed;
+    });
   }
 }
